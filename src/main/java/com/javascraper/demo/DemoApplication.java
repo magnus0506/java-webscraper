eac173e9a0651f30530aff34501fccd457a6e350
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-05-10 14:13:47 +0200

    final

--- src/main/java/com/javascraper/demo/DemoApplication.java
+++ src/main/java/com/javascraper/demo/DemoApplication.java
@@ -20,7 +20,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws SQLException {
 
         SpringApplication.run(DemoApplication.class, args);
 
@@ -44,11 +44,6 @@ public class DemoApplication {
             for (Element row : document.select("div.media-item__content")) {
 
 
-                writer2 = new BufferedWriter(new FileWriter("C:/Users/Abdul/Documents/test4.txt"));
-
-                writer3 = new BufferedWriter(new FileWriter("C:/Users/Abdul/Documents/test5.txt"));
-
-                writer4 = new BufferedWriter(new FileWriter("C:/Users/Abdul/Documents/test6.txt"));
 
                 String ticker = row.select("span.card-job-find-list__position").text();
 
@@ -57,40 +52,11 @@ public class DemoApplication {
 
                 String ticker4 = row.select("div.bullet-inline-list span:nth-child(3)").text();
 
-                writer.write(ticker);
-
-                writer2.write(ticker2);
-
-                writer2.close();
-
-
-
-                writer3.close();
-
-                writer4.write(ticker4);
-
-
-
-                writer.close();
 
                 // Inserting values
                 String query = "INSERT INTO jobs(jobtitle, company, location, jobtype) VALUES (?, ?, ?, ?)";
 
                 PreparedStatement pstmt = con.prepareStatement(query);
-
-                FileReader reader = new FileReader("C:/Users/Abdul/Documents/test2.txt");
-
-
-
-                FileReader reader3 = new FileReader("C:/Users/Abdul/Documents/test5.txt");
-
-                FileReader reader4 = new FileReader("C:/Users/Abdul/Documents/test6.txt");
-
-                pstmt.setCharacterStream(1, reader);
-
-                pstmt.setCharacterStream(2, reader2);
-
-                pstmt.setCharacterStream(3, reader3);
 
 
                 pstmt.execute();
