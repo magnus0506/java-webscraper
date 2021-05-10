9d2e1d183eb8366615a5fda5dfa7862ccc35a2d5
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-05-10 14:35:30 +0200

    error fixed

--- src/main/java/com/javascraper/demo/DemoApplication.java
+++ src/main/java/com/javascraper/demo/DemoApplication.java
@@ -1,3 +1,5 @@
+
+
 package com.javascraper.demo;
 
 import java.io.BufferedWriter;
@@ -20,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws IOException, SQLException {
 
         SpringApplication.run(DemoApplication.class, args);
 
@@ -43,21 +45,58 @@ public class DemoApplication {
 
             for (Element row : document.select("div.media-item__content")) {
 
+                writer = new BufferedWriter(new FileWriter("test2.txt"));
 
+                writer2 = new BufferedWriter(new FileWriter("test4.txt"));
+
+                writer3 = new BufferedWriter(new FileWriter("test5.txt"));
+
+                writer4 = new BufferedWriter(new FileWriter("test6.txt"));
 
                 String ticker = row.select("span.card-job-find-list__position").text();
 
                 String ticker2 = row.select("div.bullet-inline-list span:nth-child(1)").text();
 
+                String ticker3 = row.select("div.bullet-inline-list span:nth-child(2)").text();
 
                 String ticker4 = row.select("div.bullet-inline-list span:nth-child(3)").text();
 
+                writer.write(ticker);
+
+                writer2.write(ticker2);
+
+                writer2.close();
+
+                writer3.write(ticker3);
+
+                writer3.close();
+
+                writer4.write(ticker4);
+
+                writer4.close();
+
+                writer.close();
 
                 // Inserting values
                 String query = "INSERT INTO jobs(jobtitle, company, location, jobtype) VALUES (?, ?, ?, ?)";
 
                 PreparedStatement pstmt = con.prepareStatement(query);
 
+                FileReader reader = new FileReader("test2.txt");
+
+                FileReader reader2 = new FileReader("test4.txt");
+
+                FileReader reader3 = new FileReader("test5.txt");
+
+                FileReader reader4 = new FileReader("test6.txt");
+
+                pstmt.setCharacterStream(1, reader);
+
+                pstmt.setCharacterStream(2, reader2);
+
+                pstmt.setCharacterStream(3, reader3);
+
+                pstmt.setCharacterStream(4, reader4);
 
                 pstmt.execute();
 
@@ -66,6 +105,7 @@ public class DemoApplication {
                 System.out.println("Data inserted......");
             }
 
+            System.out.println("Data inserted......");
 
             System.out.println(document.outerHtml());
         } catch (Exception ex) {
